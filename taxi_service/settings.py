d32bdb7800e0045e54f63b4fd68282bca1a112bc
Roman <roma.eksit@gmail.com>
2026-06-19 03:01:33 +0200

    Remove static root

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -124,8 +124,6 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATIC_ROOT = BASE_DIR / "static"
-
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -22,4 +22,3 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("taxi.urls", namespace="taxi")),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
